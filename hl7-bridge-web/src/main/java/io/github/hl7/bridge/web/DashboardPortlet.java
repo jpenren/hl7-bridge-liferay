3a6e061738f4d268ec51da13f455315613d159f6
jpenren <javiprendo@gmail.com>
2016-04-27 11:50:26 +0200

    Fixes for Liferay7-ga1

--- hl7-bridge-web/src/main/java/io/github/hl7/bridge/web/DashboardPortlet.java
+++ hl7-bridge-web/src/main/java/io/github/hl7/bridge/web/DashboardPortlet.java
@@ -15,13 +15,13 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 @Component(
 	immediate = true,
 	property = {
-		"javax.portlet.name=DashboardPortlet",
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.security-role-ref=power-user,user",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language"
+			"com.liferay.portlet.display-category=category.sample",
+			"com.liferay.portlet.instanceable=true",
+			"javax.portlet.display-name=DashboardPortlet",
+			"javax.portlet.init-param.template-path=/",
+			"javax.portlet.init-param.view-template=/view.jsp",
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
 )
